9482977afb01afa19652788460d285c0721fbd54
yu-teacher <119153725+yu-teacher@users.noreply.github.com>
2024-02-18 04:29:35 +0900

    Update ProApplicationTests.java
    깃허브 액션 테스트용

--- src/test/java/com/ArtSeeReal/pro/ProApplicationTests.java
+++ src/test/java/com/ArtSeeReal/pro/ProApplicationTests.java
@@ -3,11 +3,7 @@ package com.ArtSeeReal.pro;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+class ProApplicationTests {
 
 }
