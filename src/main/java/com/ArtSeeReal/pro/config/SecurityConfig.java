37b0f640542fb7ab1111a64566a502ab16b09922
HyoYoungJung <hy8grace@gmail.com>
2024-03-17 20:32:05 +0900

    feat : 스프링시큐리티 설정v1 - 부트 제공 임시 pw로 로그인

--- src/main/java/com/ArtSeeReal/pro/config/SecurityConfig.java
+++ src/main/java/com/ArtSeeReal/pro/config/SecurityConfig.java
@@ -19,13 +19,14 @@ public class SecurityConfig {
         http.csrf().disable().cors().disable()
                 .authorizeHttpRequests(request -> request
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-//                        .requestMatchers("/resources/**").permitAll()  // 로그인 안해도 접속 가능하게 예외 처리
+                        .requestMatchers("/images/**","/view/login", "/view/join").permitAll()  // 로그인 안해도 접속 가능하게 예외 처리
                         .anyRequest().authenticated() // 어떠한 요청이라도 인증 필요
                 )
                 .formLogin(login -> login // form 방식 로그인 사용
                         .loginPage("/view/login") // 커스텀 로그인 페이지 지정
-                        .usernameParameter("username") // submit할 아이디
-                        .passwordParameter("password") // submit할 비밀번
+                        .loginProcessingUrl("/login") // 로그인 처리 URL 지정
+                        .usernameParameter("userid") // submit할 아이디
+                        .passwordParameter("pw") // submit할 비밀번
                         .defaultSuccessUrl("/view/dashboard", true) // 성공 시 성공페이지로 이동
                         .permitAll() // 로그인 성공페이지 이동이 막히면 안되므로 허용
                 )
