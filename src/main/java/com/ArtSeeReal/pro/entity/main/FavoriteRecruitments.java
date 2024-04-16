8e6a865f07235b03ea784166466512afe798d33b
yu-teacher <yusm1231@gmail.com>
2024-04-16 20:56:40 +0900

    task : 공고 스크랩, 취소 기능 1차

---
+++ src/main/java/com/ArtSeeReal/pro/entity/main/FavoriteRecruitments.java
@@ -0,0 +1,18 @@
+package com.ArtSeeReal.pro.entity.main;
+
+import com.ArtSeeReal.pro.entity.composite.FavoritePortfolioKey;
+import com.ArtSeeReal.pro.entity.composite.FavoriteRecruitmentKey;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "FAVORITE_RECRUITMENT")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteRecruitments {
+    @EmbeddedId
+    private FavoriteRecruitmentKey pk;
+}
